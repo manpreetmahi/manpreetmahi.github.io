580a4442d72b0123d3f32a50951d3ce2faaca160
soarr <528184@pdsb.net>
2018-04-17 20:18:01 -0400

    stufz

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ particlesJS('particles-js',
   {
     "particles": {
       "number": {
-        "value": 80,
+        "value": 10,
         "density": {
           "enable": true,
           "value_area": 800
@@ -29,7 +29,7 @@ particlesJS('particles-js',
         "type": "circle",
         "stroke": {
           "width": 0,
-          "color": "#000000"
+          "color": "#00aa00"
         },
         "polygon": {
           "nb_sides": 5
